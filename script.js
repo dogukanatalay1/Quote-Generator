f43735a69d897b0773bacbddfe05caeba52fab16
Doğukan <dogukanatalay46@gmail.com>
2022-01-07 02:38:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const quoteText = document.getElementById('quote-text'),
+      quoteTags = document.getElementById('quote-tags'),
+      quoteAuthor = document.getElementById('quote-author'),
+      genQuoteBtn = document.getElementById('gen-quote-btn');
+    
+function randomQuote() {
+    
+    fetch('https://api.quotable.io/random')
+    .then(response => response.json())
+    .then(data => {
+        quoteText.textContent = data.content;
+        quoteTags.textContent = data.tags;
+        quoteAuthor.textContent = `-- ${data.author}`;
+    });    
+}
+randomQuote();
+
+genQuoteBtn.addEventListener('click', function() {
+   randomQuote();
+});
